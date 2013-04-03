1f6e13272091e240530683bf7ccc1edc74684d87
Cyber-host <locallhost@bk.ru>
2013-04-03 22:50:51 +0300

    ppts

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+        session_start();
+
+        // встановлюемо display_errors в позицiю 1, для вiдображення синтаксичних помилок в браузерi
+	ini_set('display_errors', 1);
+
+	require_once 'application/bootstrap.php';
+
+
+	// change text for git status!
